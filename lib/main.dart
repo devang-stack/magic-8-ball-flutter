477b30aaed9097754ca36ae61ac6628b0ec9b627
Devang Mathur <masterdevang@gmail.com>
2021-07-20 01:42:30 +0530

    feat:state added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(child: Text("Magic 8 ball(Ask Me Anything)")),
+              backgroundColor: Colors.blue[800],
+            ),
+            backgroundColor: Colors.blue[200],
+            body: MagicBall()),
       ),
     );
+
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+int ball= 1;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          child: Image.asset('images/ball$ball.png'),
+          onPressed: () {
+          
+  setState(() {
+      ball= Random().nextInt(5)+1;
+           },
+          );
+         },
+          
+        ),
+      ),
+    );
+  }
+}
